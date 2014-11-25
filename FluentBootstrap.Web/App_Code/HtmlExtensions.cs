ac385dd5ce5aa1c44e13a7b78d2f351b3e018946
Dave Glick <dave@somedave.com>
2014-11-25 16:12:04 -0500

    Additional CSS documentation - finished grid system and typography

---
+++ FluentBootstrap.Web/App_Code/HtmlExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages.Html;
+
+public static class HtmlExtensions
+{
+    public static IHtmlString Code(this HtmlHelper htmlHelper, string code, string language = "csharp")
+    {
+        return new HtmlString(string.Format(@"<pre><code class=""language-{0}"">{1}</code></pre>", language, 
+            htmlHelper.Raw(HttpUtility.HtmlEncode(code.Replace("Bootstrap(this)", "Bootstrap()")))));
+    }
+}
+
+
